88304cff88b42e6d9c9ee9e4b9ae65ec83d974a4
yoontaebin <gusckd000@gmail.com>
2023-12-12 12:13:35 +0900

    update :: PostReposity -> GroupRepository로 변경

--- src/main/kotlin/team/kin/forest/domain/group/persistence/repository/GroupRepository.kt
+++ src/main/kotlin/team/kin/forest/domain/group/persistence/repository/GroupRepository.kt
@@ -1,7 +1,7 @@
 package team.kin.forest.domain.group.persistence.repository
 
 import org.springframework.data.repository.CrudRepository
-import team.kin.forest.domain.post.persistence.entity.PostJpaEntity
+import team.kin.forest.domain.group.persistence.entity.GroupJpaEntity
 import java.util.UUID
 
-interface PostReposity : CrudRepository<PostJpaEntity, UUID>
+interface GroupRepository : CrudRepository<GroupJpaEntity, UUID>
