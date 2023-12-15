d655c4bf7e43f19e2958ed6a72ee1423bad8f255
yoontaebin <gusckd000@gmail.com>
2023-12-15 15:05:00 +0900

    create :: UserNotFoundException 추가

--- src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
+++ src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
@@ -7,7 +7,7 @@ enum class ErrorCode(
 
     // USER
     DUPLICATE_USER_EMAIL("중복된 이메일 입니다.", 409),
-
+    USER_NOT_FOUND("계정을 찾을 수 없습니다.", 404),
 
     // AUTHENTICATION
     AUTHENTICATION_NOT_FOUND("인증되지 않은 사용자 입니다.", 404)
