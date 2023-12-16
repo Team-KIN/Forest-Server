ed04aed87f7267cbf9e510ffac01001724b45416
ani2689 <ojno7777@gmail.com>
2023-12-16 23:55:47 +0900

    add :: endpoint 추가

--- src/main/kotlin/team/kin/forest/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/team/kin/forest/global/security/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package team.kin.forest.global.security.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -28,6 +29,8 @@ class SecurityConfig {
     private fun authorizeHttpRequests(http: HttpSecurity) {
         http.authorizeHttpRequests()
             .mvcMatchers("/auth/**").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/group").authenticated()
+            .mvcMatchers(HttpMethod.GET, "/group/{id}").authenticated()
     }
 
     private fun exceptionHandling(http: HttpSecurity) {
