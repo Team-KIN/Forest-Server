3abdb259cf291695481e9558425769d631e8531d
yoontaebin <gusckd000@gmail.com>
2023-12-16 23:48:36 +0900

    create :: CommandRefreshTokenPersistence

---
+++ src/main/kotlin/team/kin/forest/domain/user/adapter/output/persistence/CommandRefreshTokenPersistenceAdapter.kt
@@ -0,0 +1,20 @@
+package team.kin.forest.domain.user.adapter.output.persistence
+
+import org.springframework.stereotype.Component
+import team.kin.forest.domain.user.adapter.output.persistence.mapper.RefreshTokenMapper
+import team.kin.forest.domain.user.adapter.output.persistence.repository.RefreshTokenRepository
+import team.kin.forest.domain.user.application.port.output.CommandRefreshTokenPort
+import team.kin.forest.domain.user.domain.RefreshToken
+
+@Component
+class CommandRefreshTokenPersistenceAdapter(
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val refreshTokenMapper: RefreshTokenMapper
+): CommandRefreshTokenPort {
+
+    override fun saveRefreshToken(refreshToken: RefreshToken): String {
+        val refreshTokenEntity = refreshTokenMapper.toEntity(refreshToken)
+        return refreshTokenRepository.save(refreshTokenEntity).refreshToken
+    }
+
+}
