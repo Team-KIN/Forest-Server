e7227ae9fc36ea78299fb7229d9f7c20fb80c542
ani2689 <ojno7777@gmail.com>
2023-12-17 13:30:59 +0900

    create :: group usecase&service

---
+++ src/main/kotlin/team/kin/forest/domain/group/service/QueryGroupsService.kt
@@ -0,0 +1,33 @@
+package team.kin.forest.domain.group.service
+
+import team.kin.forest.common.annotation.ServiceWithReadOnlyTransaction
+import team.kin.forest.domain.group.adapter.output.persistence.enums.GroupScope
+import team.kin.forest.domain.group.application.port.input.QueryGroupsUseCase
+import team.kin.forest.domain.group.application.port.output.dto.GroupsDto
+import team.kin.forest.domain.group.application.port.output.QueryGroupPort
+import team.kin.forest.domain.group.application.port.output.QueryMemberPort
+import team.kin.forest.domain.todo.application.port.output.QueryTodoPort
+
+@ServiceWithReadOnlyTransaction
+class QueryGroupsService(
+    private val queryGroupPort: QueryGroupPort,
+    private val queryMemberPort: QueryMemberPort,
+    private val queryTodoPort: QueryTodoPort
+) : QueryGroupsUseCase {
+    override fun execute(): List<GroupsDto> {
+        val groups = queryGroupPort.findAllByGroupScope(GroupScope.PUBLIC)
+            .map {
+                val headcount = queryMemberPort.countByGroup(it)
+                val todo = queryTodoPort.countByGroup(it)
+
+                GroupsDto(
+                    id = it.id,
+                    name = it.name,
+                    headcount = headcount,
+                    todo = todo
+                )
+            }
+
+        return groups
+    }
+}
