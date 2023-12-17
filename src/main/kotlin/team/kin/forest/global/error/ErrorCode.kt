d82465f49479c143af56a17354bbbff8e7769de6
ani2689 <ojno7777@gmail.com>
2023-12-17 14:46:00 +0900

    create :: group not found

--- src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
+++ src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
@@ -9,6 +9,9 @@ enum class ErrorCode(
     DUPLICATE_ACCOUNT_EMAIL("중복된 이메일 입니다.", 409),
 
     // AUTHENTICATION
-    AUTHENTICATION_NOT_FOUND("인증되지 않은 사용자 입니다.", 404)
+    AUTHENTICATION_NOT_FOUND("인증되지 않은 사용자 입니다.", 404),
+
+    // GROUP
+    GROUP_NOT_FOUND("그룹을 찾을 수 없습니다.", 404)
 
 }
