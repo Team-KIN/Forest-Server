2065a3b707d589cd04d9eb804a5495899ad5eea0
ani2689 <ojno7777@gmail.com>
2023-12-17 14:52:54 +0900

    add :: group details endpoint

--- src/main/kotlin/team/kin/forest/domain/group/adapter/input/mapper/GroupDataMapper.kt
+++ src/main/kotlin/team/kin/forest/domain/group/adapter/input/mapper/GroupDataMapper.kt
@@ -1,7 +1,9 @@
 package team.kin.forest.domain.group.adapter.input.mapper
 
 import org.springframework.stereotype.Component
+import team.kin.forest.domain.group.adapter.input.data.response.QueryGroupDetailsResponse
 import team.kin.forest.domain.group.adapter.input.data.response.QueryGroupsResponse
+import team.kin.forest.domain.group.application.port.output.dto.GroupDetailsDto
 import team.kin.forest.domain.group.application.port.output.dto.GroupsDto
 import team.kin.forest.domain.group.adapter.input.data.response.QueryGroupsResponse.QueryGroupResponse as QueryGroupResponse
 
@@ -18,6 +20,14 @@ class GroupDataMapper {
                     todo = it.todo
                 )
             }
+        )
+
+    infix fun toResponse(dto: GroupDetailsDto) =
+        QueryGroupDetailsResponse(
+            name = dto.name,
+            content = dto.content,
+            purpose = dto.purpose,
+            headcount = dto.headcount,
         )
 
 }
