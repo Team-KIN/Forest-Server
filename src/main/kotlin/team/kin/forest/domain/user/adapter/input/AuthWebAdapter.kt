1fc224263a797c99ef80dcf9a190bacfded93ddd
yoontaebin <gusckd000@gmail.com>
2023-12-18 15:07:22 +0900

    add :: 토큰 재발급 api 추가

--- src/main/kotlin/team/kin/forest/domain/user/adapter/input/AuthWebAdapter.kt
+++ src/main/kotlin/team/kin/forest/domain/user/adapter/input/AuthWebAdapter.kt
@@ -2,14 +2,17 @@ package team.kin.forest.domain.user.adapter.input
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.kin.forest.domain.user.adapter.input.data.request.SignInRequest
 import team.kin.forest.domain.user.adapter.input.data.request.SignUpRequest
 import team.kin.forest.domain.user.adapter.input.data.response.TokenResponse
 import team.kin.forest.domain.user.adapter.input.mapper.AuthDataMapper
+import team.kin.forest.domain.user.application.port.input.ReissueTokenUseCase
 import team.kin.forest.domain.user.application.port.input.SignInUseCase
 import team.kin.forest.domain.user.application.port.input.SignUpUseCase
 import javax.validation.Valid
@@ -19,7 +22,8 @@ import javax.validation.Valid
 class AuthWebAdapter(
     private val authDataMapper: AuthDataMapper,
     private val signUpUseCase: SignUpUseCase,
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    private val reissueTokenUseCase: ReissueTokenUseCase
 ) {
 
     @PostMapping("/signup")
@@ -30,6 +34,12 @@ class AuthWebAdapter(
     @PostMapping("/signin")
     fun signIn(@RequestBody @Valid request: SignInRequest): ResponseEntity<TokenResponse> =
         signInUseCase.execute(authDataMapper toDto request)
+            .let { authDataMapper toResponse it }
+            .let { ResponseEntity.ok(it) }
+
+    @PatchMapping("/reissue")
+    fun reissueToken(@RequestHeader refreshToken: String): ResponseEntity<TokenResponse> =
+        reissueTokenUseCase.execute(refreshToken)
             .let { authDataMapper toResponse it }
             .let { ResponseEntity.ok(it) }
 
