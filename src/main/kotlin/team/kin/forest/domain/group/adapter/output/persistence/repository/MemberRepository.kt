8e6f96281699da1df8b2f56d306bf06c076f1103
ani2689 <ojno7777@gmail.com>
2023-12-18 14:10:59 +0900

    add :: 유저가 소속된 그룹 가져오기

--- src/main/kotlin/team/kin/forest/domain/group/adapter/output/persistence/repository/MemberRepository.kt
+++ src/main/kotlin/team/kin/forest/domain/group/adapter/output/persistence/repository/MemberRepository.kt
@@ -2,10 +2,14 @@ package team.kin.forest.domain.group.adapter.output.persistence.repository
 
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import team.kin.forest.domain.group.adapter.output.persistence.entity.MemberJpaEntity
 import team.kin.forest.domain.post.adapter.output.persistence.entity.PostJpaEntity
 import java.util.UUID
 
 interface MemberRepository  : CrudRepository<PostJpaEntity, Long> {
     @Query("SELECT COUNT(m) FROM MemberJpaEntity m WHERE m.group.id = :groupId")
     fun countByGroupId(groupId: UUID): Int
+
+    @Query("SELECT m FROM MemberJpaEntity m WHERE m.user.id = :userId")
+    fun findAllByUserId(userId: UUID): List<MemberJpaEntity>
 }
