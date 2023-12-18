ca5de125013f9010da9aeab9536efcb1ef62aba1
ani2689 <ojno7777@gmail.com>
2023-12-18 20:48:35 +0900

    create :: create group request & response

---
+++ src/main/kotlin/team/kin/forest/domain/group/adapter/input/data/request/CreateGroupRequest.kt
@@ -0,0 +1,21 @@
+package team.kin.forest.domain.group.adapter.input.data.request
+
+import org.hibernate.validator.constraints.Length
+import team.kin.forest.domain.group.adapter.output.persistence.enums.GroupScope
+import javax.validation.constraints.NotBlank
+
+data class CreateGroupRequest(
+    @field:NotBlank(message = "그룹 이름은 필수값입니다.")
+    @field:Length(min = 2, max = 40)
+    private val name: String,
+
+    @field:NotBlank(message = "그룹 설명은 필수값입니다.")
+    @field:Length(max = 100)
+    private val content: String,
+
+    @field:NotBlank(message = "그룹 목적은 필수값입니다.")
+    @field:Length(max = 100)
+    private val purpose: String,
+
+    private val groupScope: GroupScope
+)
