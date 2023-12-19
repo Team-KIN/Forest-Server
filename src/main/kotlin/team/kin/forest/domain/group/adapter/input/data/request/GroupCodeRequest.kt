15168567a96e65498366676e42d0ed7588441f0a
ani2689 <ojno7777@gmail.com>
2023-12-19 12:16:05 +0900

    fix :: code 유효성 체크

--- src/main/kotlin/team/kin/forest/domain/group/adapter/input/data/request/GroupCodeRequest.kt
+++ src/main/kotlin/team/kin/forest/domain/group/adapter/input/data/request/GroupCodeRequest.kt
@@ -1,5 +1,8 @@
 package team.kin.forest.domain.group.adapter.input.data.request
 
+import javax.validation.constraints.NotBlank
+
 data class GroupCodeRequest (
+    @field:NotBlank
     val code: String
 )
