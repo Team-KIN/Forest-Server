0b9bc31f2a955bdb5632c822d060d46fe824c4bc
ani2689 <ojno7777@gmail.com>
2023-12-19 14:23:21 +0900

    create :: query my page usecase&service

---
+++ src/main/kotlin/team/kin/forest/domain/user/application/service/QueryMyPageService.kt
@@ -0,0 +1,24 @@
+package team.kin.forest.domain.user.application.service
+
+import team.kin.forest.common.annotation.ServiceWithReadOnlyTransaction
+import team.kin.forest.domain.user.application.exception.UserNotFoundException
+import team.kin.forest.domain.user.application.port.input.QueryMyPageUseCase
+import team.kin.forest.domain.user.application.port.output.QueryUserPort
+import team.kin.forest.domain.user.application.port.output.dto.UserDto
+
+@ServiceWithReadOnlyTransaction
+class QueryMyPageService(
+    private val queryUserPort: QueryUserPort
+) : QueryMyPageUseCase {
+    override fun execute(): UserDto {
+        val user = queryUserPort.findCurrentUser()
+            ?: throw UserNotFoundException()
+
+        return UserDto(
+            name = user.name,
+            email = user.email,
+            profileUrl = user.profileUrl
+        )
+    }
+
+}
