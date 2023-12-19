1e7114a24d24cb5b58fc991b71dac4b2bba4c8bc
yoontaebin <gusckd000@gmail.com>
2023-12-19 09:19:01 +0900

    create :: MessageSendFailedException 추가

--- src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
+++ src/main/kotlin/team/kin/forest/global/error/ErrorCode.kt
@@ -12,7 +12,6 @@ enum class ErrorCode(
 
     // AUTHENTICATION
     AUTHENTICATION_NOT_FOUND("인증되지 않은 사용자 입니다.", 404),
-
     // GROUP
     GROUP_NOT_FOUND("그룹을 찾을 수 없습니다.", 404),
 
@@ -20,6 +19,9 @@ enum class ErrorCode(
     INVALID_TOKEN("유효하지 않은 토큰입니다.", 401),
     INVALID_TOKEN_TYPE("유효하지 않은 토큰 타입 입니다.", 401),
     EXPIRED_ACCESS_TOKEN("만료된 accessToken 입니다.", 401),
-    EXPIRED_REFRESH_TOKEN("만료된 refreshToken 입니다.", 401)
+    EXPIRED_REFRESH_TOKEN("만료된 refreshToken 입니다.", 401),
+
+    // MESSAGE
+    MESSAGE_SEND_FAILED("coolsms 메세지 전송에 실패하였습니다.", 500)
 
 }
