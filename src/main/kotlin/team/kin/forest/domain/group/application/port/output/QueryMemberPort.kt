114bd0abb6f8f413c36fb471a84482f99ba1a40e
ani2689 <ojno7777@gmail.com>
2023-12-19 11:44:29 +0900

    add :: 가입된 멤버인지 확인

--- src/main/kotlin/team/kin/forest/domain/group/application/port/output/QueryMemberPort.kt
+++ src/main/kotlin/team/kin/forest/domain/group/application/port/output/QueryMemberPort.kt
@@ -1,10 +1,13 @@
 package team.kin.forest.domain.group.application.port.output
 
 import team.kin.forest.domain.group.domain.Group
+import team.kin.forest.domain.group.domain.Member
 import java.util.UUID
 
 
 interface QueryMemberPort {
     fun countByGroupId(groupId: UUID): Int
     fun findGroupByUserId(userId: UUID): List<Group>
+
+    fun existsMember(member: Member): Boolean
 }
