d4de2280f0630502749673311434c3668f0f709a
yoontaebin <gusckd000@gmail.com>
2023-12-19 09:52:17 +0900

    add :: CoolSmsProperties 추가

--- src/main/kotlin/team/kin/forest/global/config/PropertiesScanConfig.kt
+++ src/main/kotlin/team/kin/forest/global/config/PropertiesScanConfig.kt
@@ -2,6 +2,7 @@ package team.kin.forest.global.config
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
+import team.kin.forest.domain.user.adapter.output.message.properties.CoolSmsProperties
 import team.kin.forest.global.security.token.common.JwtExpTimeProperties
 import team.kin.forest.global.security.token.common.JwtProperties
 
@@ -9,7 +10,8 @@ import team.kin.forest.global.security.token.common.JwtProperties
 @ConfigurationPropertiesScan(
     basePackageClasses = [
         JwtProperties::class,
-        JwtExpTimeProperties::class
+        JwtExpTimeProperties::class,
+        CoolSmsProperties::class
     ]
 )
 class PropertiesScanConfig
