902c963431773131c910b35ed1fd549eda1d5b35
yoontaebin <gusckd000@gmail.com>
2023-12-20 13:50:08 +0900

    create :: CommandPrivateTodoPersistence 추가

---
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/CommandPrivateTodoPersistenceAdapter.kt
@@ -0,0 +1,24 @@
+package team.kin.forest.domain.todo.adapter.output.persistence
+
+import org.springframework.stereotype.Component
+import team.kin.forest.domain.todo.adapter.output.persistence.mapper.PrivateTodoMapper
+import team.kin.forest.domain.todo.adapter.output.persistence.repository.PrivateTodoRepository
+import team.kin.forest.domain.todo.application.port.output.CommandPrivateTodoPort
+import team.kin.forest.domain.todo.domain.PrivateTodo
+
+@Component
+class CommandPrivateTodoPersistenceAdapter(
+    private val privateTodoRepository: PrivateTodoRepository,
+    private val privateTodoMapper: PrivateTodoMapper
+) : CommandPrivateTodoPort {
+
+    override fun savePrivateTodo(privateTodo: PrivateTodo) {
+        println("11111")
+        val privateTodoEntity = privateTodoMapper.toEntity(privateTodo)
+        println("222222")
+        println(privateTodoEntity.todo.id)
+        privateTodoRepository.save(privateTodoEntity)
+        println("3333333")
+    }
+
+}
