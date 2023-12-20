7f3504606d79a494ec3f717a3f4d08061ddd637b
ani2689 <ojno7777@gmail.com>
2023-12-20 11:52:32 +0900

    create :: query post persistence port&adapter

--- src/main/kotlin/team/kin/forest/domain/post/adapter/output/persistence/repository/PostRepository.kt
+++ src/main/kotlin/team/kin/forest/domain/post/adapter/output/persistence/repository/PostRepository.kt
@@ -1,7 +1,11 @@
 package team.kin.forest.domain.post.adapter.output.persistence.repository
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import team.kin.forest.domain.post.adapter.output.persistence.entity.PostJpaEntity
 import java.util.*
 
-interface PostRepository : CrudRepository<PostJpaEntity, UUID>
+interface PostRepository : CrudRepository<PostJpaEntity, UUID> {
+    @Query("SELECT p FROM PostJpaEntity p WHERE p.group.id = :groupId")
+    fun findAllByGroupId(groupId: UUID): List<PostJpaEntity>
+}
