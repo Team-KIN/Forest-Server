d6663ce13abd43fe6ef287d8a11821c014687900
yoontaebin <gusckd000@gmail.com>
2023-12-20 13:49:57 +0900

    create :: CommandTodoPersistence 추가

---
+++ src/main/kotlin/team/kin/forest/domain/todo/application/port/output/CommandTodoPort.kt
@@ -0,0 +1,9 @@
+package team.kin.forest.domain.todo.application.port.output
+
+import team.kin.forest.domain.todo.domain.Todo
+
+interface CommandTodoPort {
+
+    fun saveTodo(todo: Todo): Todo
+
+}
