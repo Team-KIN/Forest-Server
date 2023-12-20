8f98b5ea2e81a8545bc4303af61f41869c416175
yoontaebin <gusckd000@gmail.com>
2023-12-20 13:50:44 +0900

    create :: 개인 투두 작성 api 추가

---
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/input/TodoWebAdapter.kt
@@ -0,0 +1,24 @@
+package team.kin.forest.domain.todo.adapter.input
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import team.kin.forest.domain.todo.application.port.input.WritePrivateTodoUseCase
+import java.util.UUID
+
+@RestController
+@RequestMapping("/todo")
+class TodoWebAdapter(
+    private val writePrivateTodoUseCase: WritePrivateTodoUseCase
+) {
+
+    @PostMapping("/{id}/private-todo")
+    fun writePrivateTodo(@PathVariable id: UUID, @RequestBody content: String): ResponseEntity<Void> =
+        writePrivateTodoUseCase.execute(id, content)
+            .run { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+}
