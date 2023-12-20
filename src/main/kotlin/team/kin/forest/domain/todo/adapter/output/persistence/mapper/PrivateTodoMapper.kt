36849c71f602bec3c7a52a05be3a59d398831796
yoontaebin <gusckd000@gmail.com>
2023-12-20 13:49:32 +0900

    create :: PrivateTodoMapper 추가

---
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/mapper/PrivateTodoMapper.kt
@@ -0,0 +1,30 @@
+package team.kin.forest.domain.todo.adapter.output.persistence.mapper
+
+import org.springframework.stereotype.Component
+import team.kin.forest.domain.todo.adapter.output.persistence.entity.PrivateTodoJpaEntity
+import team.kin.forest.domain.todo.domain.PrivateTodo
+import team.kin.forest.domain.user.adapter.output.persistence.mapper.UserMapper
+
+@Component
+class PrivateTodoMapper(
+    private val todoMapper: TodoMapper,
+    private val userMapper: UserMapper
+) {
+
+    fun toEntity(domain: PrivateTodo): PrivateTodoJpaEntity =
+        PrivateTodoJpaEntity(
+            id = domain.id,
+            todoStatus = domain.todoStatus,
+            todo = todoMapper.toEntity(domain.todo),
+            user = userMapper.toEntity(domain.user)
+        )
+
+    fun toDomain(entity: PrivateTodoJpaEntity): PrivateTodo =
+        PrivateTodo(
+            id = entity.id,
+            todoStatus = entity.todoStatus,
+            todo = todoMapper.toDomain(entity.todo),
+            user = userMapper.toDomain(entity.user)
+        )
+
+}
