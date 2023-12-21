299b772adee58f2e0d886219d9abba656666f12d
yoontaebin <gusckd000@gmail.com>
2023-12-21 22:45:20 +0900

    create :: TodoDataMapper 추가

---
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/input/mapper/TodoDataMapper.kt
@@ -0,0 +1,36 @@
+package team.kin.forest.domain.todo.adapter.input.mapper
+
+import org.springframework.stereotype.Component
+import team.kin.forest.domain.todo.adapter.input.data.request.CreateTodoRequest
+import team.kin.forest.domain.todo.adapter.input.data.response.TodoListResponse
+import team.kin.forest.domain.todo.application.port.input.dto.CreateTodoDto
+import team.kin.forest.domain.todo.application.port.input.dto.TodoListDto
+
+@Component
+class TodoDataMapper {
+
+    infix fun toDto(request: CreateTodoRequest): CreateTodoDto =
+        CreateTodoDto(
+            content = request.content
+        )
+
+    infix fun toResponse(dto: TodoListDto): TodoListResponse =
+        TodoListResponse(
+            publicTodos = dto.publicTodos.map {
+                TodoListResponse.PublicTodoResponse(
+                    id = it.id,
+                    content = it.content,
+                    achievementRate = it.achievementRate,
+                    todoStatus = it.todoStatus
+                )
+            },
+            privateTodos = dto.privateTodos.map {
+                TodoListResponse.PrivateTodoResponse(
+                    id = it.id,
+                    content = it.content,
+                    todoStatus = it.todoStatus
+                )
+            }
+        )
+
+}
