3d69d1b68eea470c8e12ad02185bc4eb0e731258
yoontaebin <gusckd000@gmail.com>
2023-12-21 22:45:30 +0900

    add :: toDomain 추가

--- src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/mapper/TodoMapper.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/mapper/TodoMapper.kt
@@ -14,6 +14,7 @@ class TodoMapper(
         TodoJpaEntity(
             id = domain.id,
             content = domain.content,
+            todoStatus = domain.todoStatus,
             todoType = domain.todoType,
             group = groupMapper.toEntity(domain.group)
         )
@@ -22,8 +23,20 @@ class TodoMapper(
         Todo(
             id = entity.id,
             content = entity.content,
+            todoStatus = entity.todoStatus,
             todoType = entity.todoType,
             group = groupMapper.toDomain(entity.group)
         )
+
+    fun toDomain(list: List<TodoJpaEntity>): List<Todo> =
+        list.map {
+            Todo(
+                id = it.id,
+                content = it.content,
+                todoStatus = it.todoStatus,
+                todoType = it.todoType,
+                group = groupMapper.toDomain(it.group)
+            )
+        }
 
 }
