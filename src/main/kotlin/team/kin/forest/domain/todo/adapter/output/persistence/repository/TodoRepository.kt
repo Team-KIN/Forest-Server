61b72f2fadc0b1f53a795cfcb2c650a9ab4ae1af
yoontaebin <gusckd000@gmail.com>
2023-12-21 22:43:42 +0900

    add :: findAllByGroupAndTodoType 추가

--- src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/repository/TodoRepository.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/repository/TodoRepository.kt
@@ -2,10 +2,16 @@ package team.kin.forest.domain.todo.adapter.output.persistence.repository
 
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import team.kin.forest.domain.group.adapter.output.persistence.entity.GroupJpaEntity
+import team.kin.forest.domain.group.domain.Group
 import team.kin.forest.domain.todo.adapter.output.persistence.entity.TodoJpaEntity
+import team.kin.forest.domain.todo.adapter.output.persistence.enums.TodoType
+import team.kin.forest.domain.todo.domain.Todo
 import java.util.*
 
 interface TodoRepository : CrudRepository<TodoJpaEntity, UUID> {
     @Query("SELECT COUNT(t) FROM TodoJpaEntity t WHERE t.group.id = :groupId")
     fun countByGroupId(groupId: UUID): Int
+
+    fun findAllByGroupAndTodoType(group: GroupJpaEntity, todoType: TodoType): List<TodoJpaEntity>
 }
