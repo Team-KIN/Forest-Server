1e267b39970cd0d5dc6da2bf83f7b89f6f013020
yoontaebin <gusckd000@gmail.com>
2023-12-21 22:43:05 +0900

    add :: findAllByGroupAndTodoType 추가

--- src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/QueryTodoPersistenceAdapter.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/output/persistence/QueryTodoPersistenceAdapter.kt
@@ -1,15 +1,28 @@
 package team.kin.forest.domain.todo.adapter.output.persistence
 
 import org.springframework.stereotype.Component
+import team.kin.forest.domain.group.adapter.output.persistence.mapper.GroupMapper
+import team.kin.forest.domain.group.domain.Group
+import team.kin.forest.domain.todo.adapter.output.persistence.enums.TodoType
+import team.kin.forest.domain.todo.adapter.output.persistence.mapper.TodoMapper
 import team.kin.forest.domain.todo.adapter.output.persistence.repository.TodoRepository
 import team.kin.forest.domain.todo.application.port.output.QueryTodoPort
+import team.kin.forest.domain.todo.domain.Todo
 import java.util.*
 
 @Component
 class QueryTodoPersistenceAdapter(
-    private val todoRepository: TodoRepository
+    private val todoRepository: TodoRepository,
+    private val groupMapper: GroupMapper,
+    private val todoMapper: TodoMapper
 ) : QueryTodoPort {
     override fun countByGroupId(groupId: UUID): Int {
         return todoRepository.countByGroupId(groupId)
+    }
+
+    override fun findAllByGroupAndTodoType(group: Group, todoType: TodoType): List<Todo> {
+        val groupEntity = groupMapper.toEntity(group)
+        todoRepository.findAllByGroupAndTodoType(groupEntity, todoType)
+            .let { return todoMapper.toDomain(it) }
     }
 }
