a95005364cc065b83ff8c276137e837443c8781c
ani2689 <ojno7777@gmail.com>
2023-12-26 12:14:22 +0900

    add :: comment find by id

--- src/main/kotlin/team/kin/forest/domain/comment/adapter/output/persistence/QueryCommentPersistenceAdapter.kt
+++ src/main/kotlin/team/kin/forest/domain/comment/adapter/output/persistence/QueryCommentPersistenceAdapter.kt
@@ -1,5 +1,5 @@
 package team.kin.forest.domain.comment.adapter.output.persistence
-
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.kin.forest.domain.comment.adapter.output.persistence.mapper.CommentMapper
 import team.kin.forest.domain.comment.adapter.output.persistence.repository.CommentRepository
@@ -15,6 +15,11 @@ class QueryCommentPersistenceAdapter(
     override fun findAllByPostId(postId: UUID): List<Comment> {
         val commentEntities = commentRepository.findAllByPostId(postId)
         return commentMapper.toDomain(commentEntities)
+    }
+
+    override fun findByIdOrNull(id: UUID): Comment? {
+        val commentEntity = commentRepository.findByIdOrNull(id)
+        return commentEntity?.let { commentMapper.toDomain(it) }
     }
 
 }
