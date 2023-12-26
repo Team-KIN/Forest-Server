13539089068e2351385b85d3e7e7c3bd9a3acd2b
ani2689 <ojno7777@gmail.com>
2023-12-26 09:44:01 +0900

    add :: endpoint 추가

--- src/main/kotlin/team/kin/forest/domain/post/adapter/input/PostWebAdapter.kt
+++ src/main/kotlin/team/kin/forest/domain/post/adapter/input/PostWebAdapter.kt
@@ -3,6 +3,7 @@ package team.kin.forest.domain.post.adapter.input
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.kin.forest.common.annotation.MemberOnly
 import team.kin.forest.domain.post.adapter.input.data.request.CreatePostRequest
+import team.kin.forest.domain.post.adapter.input.data.request.ModifyPostRequest
 import team.kin.forest.domain.post.adapter.input.data.response.QueryPostDetailsResponse
 import team.kin.forest.domain.post.adapter.input.data.response.QueryPostsResponse
 import team.kin.forest.domain.post.adapter.input.mapper.PostDataMapper
 import team.kin.forest.domain.post.application.port.input.CreatePostUseCase
+import team.kin.forest.domain.post.application.port.input.ModifyPostUseCase
 import team.kin.forest.domain.post.application.port.input.QueryPostDetailsUseCase
 import team.kin.forest.domain.post.application.port.input.QueryPostsUseCase
 import java.util.UUID
@@ -25,6 +28,7 @@ class PostWebAdapter(
     private val queryPostsUseCase: QueryPostsUseCase,
     private val queryPostDetailsUseCase: QueryPostDetailsUseCase,
     private val createPostUseCase: CreatePostUseCase,
+    private val modifyPostUseCase: ModifyPostUseCase,
     private val postDataMapper: PostDataMapper
 ) {
     @GetMapping
@@ -45,5 +49,14 @@ class PostWebAdapter(
     @MemberOnly
     fun createPost(@PathVariable("id") groupId: UUID, @RequestBody @Valid createPostRequest: CreatePostRequest): ResponseEntity<Void> =
         createPostUseCase.execute(groupId, postDataMapper toDto createPostRequest)
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PatchMapping("/{post_id}")
+    @MemberOnly
+    fun modifyPost(
+        @PathVariable("post_id") id: UUID, @PathVariable("id") groupId: UUID,
+        @RequestBody @Valid modifyPostRequest: ModifyPostRequest
+    ): ResponseEntity<Void> =
+        modifyPostUseCase.execute(id, groupId, postDataMapper toDto modifyPostRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
