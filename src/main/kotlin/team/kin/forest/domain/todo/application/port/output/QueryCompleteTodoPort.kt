591aa41083cfef928e83ab427dd05e66a849b36c
yoontaebin <gusckd000@gmail.com>
2023-12-28 23:31:41 +0900

    update :: CompleteTodo로 변경

--- src/main/kotlin/team/kin/forest/domain/todo/application/port/output/QueryCompleteTodoPort.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/application/port/output/QueryCompleteTodoPort.kt
@@ -3,8 +3,9 @@ package team.kin.forest.domain.todo.application.port.output
 import team.kin.forest.domain.todo.domain.Todo
 import team.kin.forest.domain.user.domain.User
 
-interface QueryCompletePort {
+interface QueryCompleteTodoPort {
 
     fun countByUserAndTodo(user: User, todo: Todo?): Int
+    fun existsByTodo(todo: Todo): Boolean
 
 }
