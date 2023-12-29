dd0a9500315b9b8abc1ae6a3fa94804742748f71
yoontaebin <gusckd000@gmail.com>
2023-12-30 01:27:02 +0900

    add :: 애그리거트 어노테이션 추가

--- src/main/kotlin/team/kin/forest/domain/todo/domain/CompleteTodo.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/domain/CompleteTodo.kt
@@ -1,7 +1,9 @@
 package team.kin.forest.domain.todo.domain
 
+import team.kin.forest.common.annotation.RootAggregate
 import team.kin.forest.domain.user.domain.User
 
+@RootAggregate
 data class CompleteTodo(
     val id: Long,
     val user: User,
