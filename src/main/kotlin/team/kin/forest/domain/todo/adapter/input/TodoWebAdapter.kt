dc4eb658353080d710d7c618e470855937fa963e
yoontaebin <gusckd000@gmail.com>
2023-12-30 16:10:13 +0900

    add :: 투두 수정 url 추가

--- src/main/kotlin/team/kin/forest/domain/todo/adapter/input/TodoWebAdapter.kt
+++ src/main/kotlin/team/kin/forest/domain/todo/adapter/input/TodoWebAdapter.kt
@@ -3,6 +3,7 @@ package team.kin.forest.domain.todo.adapter.input
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.kin.forest.common.annotation.MemberOnly
 import team.kin.forest.domain.todo.adapter.input.data.request.CreateTodoRequest
+import team.kin.forest.domain.todo.adapter.input.data.request.ModifyTodoRequest
 import team.kin.forest.domain.todo.adapter.input.data.response.TodoListResponse
 import team.kin.forest.domain.todo.adapter.input.mapper.TodoDataMapper
 import team.kin.forest.domain.todo.application.port.input.*
@@ -23,6 +25,7 @@ class TodoWebAdapter(
     private val queryTodoListUseCase: QueryTodoListUseCase,
     private val completeTodoUseCase: CompleteTodoUseCase,
     private val completePrivateTodoUseCase: CompletePrivateTodoUseCase,
+    private val modifyTodoUseCase: ModifyTodoUseCase,
     private val todoDataMapper: TodoDataMapper
 ) {
 
@@ -53,5 +56,16 @@ class TodoWebAdapter(
     fun completePrivateTodo(@PathVariable("id") groupId: UUID, @PathVariable("todo_id") todoId: UUID): ResponseEntity<Void> =
         completePrivateTodoUseCase.execute(groupId, todoId)
             .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @PatchMapping("/{id}/todo/{todo_id}")
+    fun modifyTodo(
+        @PathVariable("id") groupId: UUID,
+        @PathVariable("todo_id") todoId: UUID,
+        @RequestBody request: ModifyTodoRequest
+    ): ResponseEntity<Void> =
+        todoDataMapper.toDto(request)
+            .let { modifyTodoUseCase.execute(groupId, todoId, it) }
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
 
 }
