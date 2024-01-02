aa32cad562f6302eda0e7f3d29e3c09cc8a58b1f
yoontaebin <gusckd000@gmail.com>
2024-01-02 12:07:50 +0900

    create :: QueryGroupDetailsResponse 추가

--- src/main/kotlin/team/kin/forest/domain/group/adapter/input/data/response/QueryGroupDetailsResponse.kt
+++ src/main/kotlin/team/kin/forest/domain/group/adapter/input/data/response/QueryGroupDetailsResponse.kt
@@ -1,8 +1,18 @@
 package team.kin.forest.domain.group.adapter.input.data.response
 
-data class QueryGroupDetailsResponse (
-    val name: String,
+import java.util.UUID
+
+data class QueryGroupDetailsResponse(
     val content: String,
     val purpose: String,
-    val headcount: Int
-)
+    val code: String,
+    val users: List<MemberListResponse>
+) {
+
+    data class MemberListResponse(
+        val id: UUID,
+        val name: String,
+        val profileUrl: String
+    )
+
+}
